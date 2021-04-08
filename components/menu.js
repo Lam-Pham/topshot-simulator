6237238a126cb061fbf79f9f6663a48645c3288d
Lam-Pham <lpham127@gmail.com>
2021-04-07 17:26:16 -0700

    sized up menu

--- components/menu.js
+++ components/menu.js
@@ -16,23 +16,23 @@ export default function Menu() {
                 <div class="border-b border-black"></div>
             </div>
             <div class="space-y-0">
-                <div class="cursor-pointer 2xl:text-xl text-base hover:opacity-50">
-                    <Scrollspy offset={300} componentTag="div" items={ ['runDown'] } currentClassName="font-bold cursor-pointer 2xl:text-xl text-base hover:opacity-50">
+                <div class="cursor-pointer 2xl:text-2xl text-lg hover:opacity-50">
+                    <Scrollspy offset={300} componentTag="div" items={ ['runDown'] } currentClassName="font-bold cursor-pointer 2xl:text-2xl text-lg hover:opacity-50">
                         <Link to="runDown" spy={true} smooth={true} offset={-40}>run-down</Link>
                     </Scrollspy>
                 </div>
-                <div class="cursor-pointer 2xl:text-xl text-base hover:opacity-50">
-                    <Scrollspy offset={400} componentTag="div" items={ ['drop'] } class="cursor-pointer 2xl:text-xl text-base hover:opacity-50" currentClassName="font-bold cursor-pointer 2xl:text-xl text-base hover:opacity-50">
+                <div class="cursor-pointer 2xl:text-2xl text-lg hover:opacity-50">
+                    <Scrollspy offset={400} componentTag="div" items={ ['drop'] }currentClassName="font-bold cursor-pointer 2xl:text-2xl text-lg hover:opacity-50">
                         <Link to="drop" spy={true} smooth={true} offset={-40}>simulate drop</Link>
                     </Scrollspy>
                 </div>
-                <div class="cursor-pointer 2xl:text-xl text-base hover:opacity-50">
-                    <Scrollspy offset={600} componentTag="div" items={ ['pack'] } currentClassName="font-bold cursor-pointer 2xl:text-xl text-base hover:opacity-50">
+                <div class="cursor-pointer 2xl:text-2xl text-lg hover:opacity-50">
+                    <Scrollspy offset={600} componentTag="div" items={ ['pack'] } currentClassName="font-bold cursor-pointer 2xl:text-2xl text-lg hover:opacity-50">
                         <Link to="pack" smooth={true} offset={-40}>simulate pack</Link>
                     </Scrollspy>
                 </div>
-                <div class="cursor-pointer 2xl:text-xl text-base hover:opacity-50">
-                    <Scrollspy offset={500} componentTag="div" items={ ['founders'] } currentClassName="font-bold cursor-pointer 2xl:text-xl text-base hover:opacity-50">
+                <div class="cursor-pointer 2xl:text-2xl text-lg hover:opacity-50">
+                    <Scrollspy offset={500} componentTag="div" items={ ['founders'] } currentClassName="font-bold cursor-pointer 2xl:text-2xl text-lg hover:opacity-50">
                         <Link to="founders" smooth={true} offset={-40}>founders</Link>
                     </Scrollspy>
                 </div>
