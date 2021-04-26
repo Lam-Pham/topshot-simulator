daccce5cffaeee7fbd60ce7f91158867ecec540e
Lam-Pham <lpham127@gmail.com>
2021-04-26 12:40:38 -0700

    added unique key prop to Person

--- components/person.js
+++ components/person.js
@@ -3,7 +3,7 @@
 export default function Person(props) {
     let style = props.color + " col-span-1 text-sm border-2 border-b-4 border-black py-8 pl-8"
     return (
-        <p class={style}>
+        <p class={style} key={id}>
             <span class="font-bold">{props.username}</span><br/><br/>
             <span class="font-bold">favorite TS {props.type}</span><br/>
             {props.info} <br/><br/>
