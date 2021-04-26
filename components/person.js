0cccb83dfe08d3076947b0672d46f579e0eea6b1
Lam-Pham <lpham127@gmail.com>
2021-04-25 17:24:45 -0700

    rendered founders with map; added Person component

---
+++ components/person.js
@@ -0,0 +1,13 @@
+
+
+export default function Person(props) {
+    let style = props.color + " col-span-1 text-sm border-2 border-b-4 border-black py-8 pl-8"
+    return (
+        <p class={style}>
+            <span class="font-bold">{props.username}</span><br/><br/>
+            <span class="font-bold">favorite TS {props.type}</span><br/>
+            {props.info} <br/><br/>
+            <span class="font-bold">"{props.message}"</span>
+        </p>
+    )
+}
